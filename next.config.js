257208d54ef31cd0c681b48fd02695271c27626c
chepti <chepti@gmail.com>
2025-05-13 23:20:45 +0300

    Initial commit of Next.js boilerplate app

---
+++ next.config.js
@@ -0,0 +1,25 @@
+/** @type {import('next').NextConfig} */
+
+const withBundleAnalyzer = process.env.ANALYZE === 'true'
+  ? require('@next/bundle-analyzer')({
+      enabled: true,
+    })
+  : (config) => config;
+
+const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    domains: [
+      'img.clerk.com', // For Clerk user images
+    ],
+  },
+  // Enable SWC minification
+  compiler: {
+    // Enables emotion for styled components
+    emotion: false,
+    // Remove console.log in production
+    removeConsole: process.env.NODE_ENV === 'production',
+  },
+};
+
+module.exports = withBundleAnalyzer(nextConfig); 
